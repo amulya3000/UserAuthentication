5bc8d64d46e038d89f611b28897739f9cffc12a9
amulya3000 <amulyapushpa143@gmail.com>
2026-07-01 09:03:02 +0545

    route for MiddleWare

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\TestUser;
+use App\Http\Middleware\ValidUser;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { 
     return view('landing');
@@ -15,9 +17,18 @@ Route::get('/home', function () {
     return redirect()->route('dashboard');
 });
 
-Route::view('register', 'register')->name('register');
+Route::view('register', 'register')->name('register')->middleware(ValidUser::class);
+
+
 Route::post('registerSave', [UserController::class, 'registration'])->name('registerSave');
+
+
 Route::view('login', 'login')->name('login');
 Route::post('login', [UserController::class, 'Login'])->name('loginMatch'); 
-Route::get('dashboard', [UserController::class, 'dashboardPage'])->name('dashboard');
+
+Route::get('dashboard', [UserController::class, 'dashboardPage'])
+    ->name('dashboard')
+    ->middleware('IsUserValid',TestUser::class);
+
 Route::post('logout', [UserController::class, 'logout'])->name('logout');
+ 
